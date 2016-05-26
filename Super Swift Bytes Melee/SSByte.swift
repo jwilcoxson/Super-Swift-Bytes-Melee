7bf95867f12df27324edf01956f29024525bad2c
Joe Wilcoxson <joe.wilcoxson@gmail.com>
2016-05-25 22:01:51 -0400

    Added more documentation

--- Super Swift Bytes Melee/SSByte.swift
+++ Super Swift Bytes Melee/SSByte.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// MARK: Rotate Operators
+
 //Rotate Right Operator
 infix operator >>> {associativity left}
 func >>> (lhs: UInt8, rhs: Int) -> UInt8 {
@@ -32,13 +34,13 @@ func <<< (lhs: UInt32, rhs: Int) -> UInt32 {
     return SSByte.rotateLeftDoubleWord(lhs, bits: rhs)
 }
 
+// MARK: SSByte Handler Class -
+
 /// Handler class for dealing with byte, words and bits.
 
 class SSByte {
     
-    /*
-        Byte Functions
-    */
+    // MARK: Byte Functions -
     
     /**
       Gets the value of a specific bit in a byte.
@@ -116,6 +118,13 @@ class SSByte {
         return b
     }
     
+    /**
+     Inverts all bits in a byte.
+     
+     - parameter byte: The byte to invert.
+     - returns: The inverted byte.
+     */
+    
     class func invertByte(byte: UInt8) -> UInt8
     {
         var b : UInt8 = 0x00
@@ -126,8 +135,13 @@ class SSByte {
         return b
     }
     
-    /*
-        Word Functions
+    // MARK: Word Functions -
+    
+    /**
+     Gets the lower (least significant) byte in a word.
+     
+     - parameter word: The word to get the low byte from.
+     - returns: The lower (least significant) byte of the word.
      */
     
     class func getLowByteOfWord(word: UInt16) -> UInt8
@@ -135,10 +149,25 @@ class SSByte {
         return UInt8(truncatingBitPattern: word)
     }
     
+    /**
+     Gets the higher (most significant) byte in a word.
+     
+     - parameter word: The word to get the high byte from.
+     - returns: The higher (most significant) byte of the word.
+     */
+    
     class func getHighByteOfWord(word: UInt16) -> UInt8
     {
         return UInt8(truncatingBitPattern: (word >> 8))
     }
+    
+    /**
+     Gets the value of a specific bit in a word.
+     
+     - parameter word: The word to get a bit from.
+     - parameter bitNumber: The position of the bit to get (0-15).
+     - returns: The Bool value of the requested bit.
+     */
     
     class func getBitOfWord(word: UInt16, bitNumber: UInt8) -> Bool
     {
@@ -153,6 +182,15 @@ class SSByte {
             return getBitOfByte(b, bitNumber: (bitNumber % 16))
         }
     }
+    
+    /**
+     Sets the value of a specific bit in a word.
+     
+     - parameter word: The word to set a bit in.
+     - parameter bitNumber: The position of the bit to set (0-15).
+     - parameter value: Bool value to set the bit to.
+     - returns: The input word with the requested bit set.
+     */
     
     class func setBitOfWord(word: UInt16, bitNumber: UInt8, value: Bool) -> UInt16
     {
@@ -174,15 +212,38 @@ class SSByte {
         return wordFromBytes(highByte, lowByte: lowByte)
     }
     
+    /**
+     Assembles a word from two bytes.
+     
+     - parameter highByte: The higher byte of the word.
+     - parameter lowByte: The lower byte of the word.
+     - returns: The assembled word.
+     */
+    
     class func wordFromBytes(highByte: UInt8, lowByte: UInt8) -> UInt16
     {
         return (UInt16(highByte) << 8) ^ UInt16(lowByte)
     }
     
+    /**
+     Swaps the low and high bytes in a word.
+     
+     - parameter word: The word to swap bytes in.
+     - returns: The input word with bytes swapped.
+     */
+    
     class func swapBytesInWord(word: UInt16) -> UInt16
     {
         return wordFromBytes(getLowByteOfWord(word), lowByte: getHighByteOfWord(word))
     }
+    
+    /**
+     Rotates a word left by a specified number of bits.
+     
+     - parameter word: The word to rotate.
+     - parameter bits: The number of bits to rotate by.
+     - returns: The rotated word.
+     */
     
     class func rotateLeftWord(word: UInt16, bits: Int) -> UInt16
     {
@@ -191,12 +252,27 @@ class SSByte {
         return UInt16(truncatingBitPattern: d) ^ UInt16(truncatingBitPattern: (d >> 16))
     }
     
+    /**
+     Rotates a word right by a specified number of bits.
+     
+     - parameter word: The word to rotate.
+     - parameter bits: The number of bits to rotate by.
+     - returns: The rotated word.
+     */
+    
     class func rotateRightWord(word: UInt16, bits: Int) -> UInt16
     {
         let d = (UInt32(word) << 16) >> UInt32(bits % 16)
         
         return UInt16(truncatingBitPattern: d) ^ UInt16(truncatingBitPattern: (d >> 16))
     }
+    
+    /**
+     Reverses the order of the bits in a word.
+     
+     - parameter word: The word to reverse.
+     - returns: The reversed word.
+     */
     
     class func reverseWord(word: UInt16) -> UInt16
     {
@@ -208,6 +284,13 @@ class SSByte {
         return newWord
     }
     
+    /**
+     Inverts all bits in a word.
+     
+     - parameter word: The word to invert.
+     - returns: The inverted word.
+     */
+    
     class func invertWord(word: UInt16) -> UInt16
     {
         let lowByte = invertByte(getLowByteOfWord(word))
@@ -215,18 +298,39 @@ class SSByte {
         return wordFromBytes(highByte, lowByte: lowByte)
     }
     
-    /*
-        Double Word Functions
+    // MARK: Double Word Functions -
+    
+    /**
+     Gets the lower (least significant) word in a double word.
+     
+     - parameter double: The double word to get the low word from.
+     - returns: The lower (least significant) word of the double word.
      */
+    
     class func getLowWordOfDoubleWord(double: UInt32) -> UInt16
     {
         return UInt16(truncatingBitPattern: double)
     }
     
+    /**
+     Gets the higher (most significant) word in a double word.
+     
+     - parameter double: The double word to get the high word from.
+     - returns: The higher (most significant) word of the double word.
+     */
+    
     class func getHighWordOfDoubleWord(double: UInt32) -> UInt16
     {
         return UInt16(truncatingBitPattern: (double >> 16))
     }
+    
+    /**
+     Gets the value of a specific bit in a double word.
+     
+     - parameter double: The double word to get a bit from.
+     - parameter bitNumber: The position of the bit to get (0-31).
+     - returns: The Bool value of the requested bit.
+     */
     
     class func getBitOfDoubleWord(double: UInt32, bitNumber: UInt8) -> Bool
     {
@@ -241,6 +345,15 @@ class SSByte {
             return getBitOfWord(w, bitNumber: (bitNumber % 32))
         }
     }
+    
+    /**
+     Sets the value of a specific bit in a double word.
+     
+     - parameter double: The double word to set a bit in.
+     - parameter bitNumber: The position of the bit to set (0-31).
+     - parameter value: Bool value to set the bit to.
+     - returns: The input double word with the requested bit set.
+     */
     
     class func setBitOfDoubleWord(double: UInt32, bitNumber: UInt8, value: Bool) -> UInt32
     {
@@ -263,10 +376,25 @@ class SSByte {
         
     }
     
+    /**
+     Assembles a double word from two words.
+     
+     - parameter highWord: The higher word of the double word.
+     - parameter lowWord: The lower word of the double word.
+     - returns: The assembled double word.
+     */
+    
     class func doubleWordFromWords(highWord: UInt16, lowWord: UInt16) -> UInt32
     {
         return (UInt32(highWord) << 16) ^ UInt32(lowWord)
     }
+    
+    /**
+     Swaps the low and high word in a double word.
+     
+     - parameter double: The doublw word to swap words in.
+     - returns: The input double word with words swapped.
+     */
     
     class func swapWordsInDoubleWord(double: UInt32) -> UInt32
     {
@@ -274,6 +402,14 @@ class SSByte {
         let highWord = getHighWordOfDoubleWord(double)
         return doubleWordFromWords(lowWord, lowWord: highWord)
     }
+    
+    /**
+     Rotates a double word left by a specified number of bits.
+     
+     - parameter double: The double word to rotate.
+     - parameter bits: The number of bits to rotate by.
+     - returns: The rotated double word.
+     */
     
     class func rotateLeftDoubleWord(double: UInt32, bits: Int) -> UInt32
     {
@@ -283,12 +419,27 @@ class SSByte {
         return UInt32(truncatingBitPattern: q) ^ UInt32(truncatingBitPattern: (q >> 32))
     }
     
+    /**
+     Rotates a double word right by a specified number of bits.
+     
+     - parameter double: The double word to rotate.
+     - parameter bits: The number of bits to rotate by.
+     - returns: The rotated double word.
+     */
+    
     class func rotateRightDoubleWord(double: UInt32, bits: Int) -> UInt32
     {
         let q = (UInt64(double) << 32) >> UInt64(bits % 32)
         
         return UInt32(truncatingBitPattern: q) ^ UInt32(truncatingBitPattern: (q >> 32))
     }
+    
+    /**
+     Reverses the order of the bits in a double word.
+     
+     - parameter double: The double word to reverse.
+     - returns: The reversed double word.
+     */
     
     class func reverseDoubleWord(double: UInt32) -> UInt32
     {
@@ -300,12 +451,28 @@ class SSByte {
         return newDouble
     }
     
+    /**
+     Inverts all bits in a double word.
+     
+     - parameter double: The double word to invert.
+     - returns: The inverted double word.
+     */
+    
     class func invertDoubleWord(double: UInt32) -> UInt32
     {
         let lowWord = invertWord(getLowWordOfDoubleWord(double))
         let highWord = invertWord(getHighWordOfDoubleWord(double))
         return doubleWordFromWords(highWord, lowWord: lowWord)
     }
+    
+    /**
+     Converts a double word to a float value, *maintains bit representation in memory*.
+     
+     - parameter double: The double word to convert.
+     - returns: The float value with the same bit representation in memory.
+     */
+    
+    // TODO: Check and update for NaN values.
     
     class func doubleWordToFloat(double: UInt32) -> Float
     {
